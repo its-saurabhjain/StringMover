0ad7166cdd1aba14d4788533c176f91c5db6ba15
Saurabh Jain <Saurabh.Jain2@xerox.com>
2016-11-29 11:28:19 -0500

    Code updated for building  StringMover.jar

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,4 @@ copy lib\commons-logging-1.0.3\*.jar deploy
 copy lib\jakarta-log4j-1.2.8\dist\lib\*.jar deploy
 copy lib\ibm-mq-for-java\*.jar deploy
 copy lib\*.dll deploy
-jar cmfv manifest.info deploy\fans.jar fans.properties log4j.properties runNoJar.bat README.txt -C build .
+jar cmfv manifest.info deploy\StringMover.jar StringMover.properties log4j.properties runNoJar.bat README.txt -C build .
